2b9e9251e074b5a9858e47f9ec5915a3bba35dba
GTU9 <107167326+GTU9@users.noreply.github.com>
2026-05-11 23:40:33 +0900

    CORS 호스팅 주소 받을수 있게 변경

--- src/main/java/com/senials/security/config/WebConfig.java
+++ src/main/java/com/senials/security/config/WebConfig.java
@@ -1,16 +1,21 @@
 package com.senials.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("addCorsMappings 메소드 작동확인");
-        registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000") // 클라이언트의 URL
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
